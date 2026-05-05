33b08d063b1ed744772c070ba102e8e481dfd113
Alexander Neo <AlexanderNeo427@gmail.com>
2026-05-05 21:03:37 +0800

    Setup "food service" DB schema
    Had to swap the docker database image with one where PostGIS is installed on the system.
    
    Later will have to transfer to a migration-based workflow with Flyway.....

---
+++ backend/food-service/src/main/java/com/salob/food_service/common/config/JpaConfig.java
@@ -0,0 +1,8 @@
+package com.salob.food_service.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {}
