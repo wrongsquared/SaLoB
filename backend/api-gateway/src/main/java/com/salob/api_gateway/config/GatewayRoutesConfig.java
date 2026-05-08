1a54e999432f602413897097bee9447d002a66d0
Alexander Neo <AlexanderNeo427@gmail.com>
2026-05-08 14:50:36 +0800

    Setup eureka discovery clients (gateway + microservices)
    SEEMS to be working - console output of the clients show successful registration with the Eureka server

--- backend/api-gateway/src/main/java/com/salob/api_gateway/config/GatewayRoutesConfig.java
+++ backend/api-gateway/src/main/java/com/salob/api_gateway/config/GatewayRoutesConfig.java
@@ -7,19 +7,19 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class GatewayRoutesConfig {
-    @Value("${USER_SERVICE_URL}")
-    private String userServiceUrl;
-
-    @Value("${FOOD_SERVICE_URL}")
-    private String foodServiceUrl;
-
+//    @Value("${USER_SERVICE_URL}")
+//    private String userServiceUrl;
+//
+//    @Value("${FOOD_SERVICE_URL}")
+//    private String foodServiceUrl;
+//
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("user-service-route", r -> r.path("/api/auth/**", "/api/users/**")
-                        .uri(userServiceUrl))
+                        .uri("lb://user-service"))
                 .route("food-service-route", r -> r.path("/api/eateries/**", "/api/foods/**")
-                        .uri(foodServiceUrl))
+                        .uri("lb://food-service"))
                 .build();
     }
 }
