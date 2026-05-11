d5395b753c7f70376a14c16a232d0abdc1c6339f
Alexander Neo <AlexanderNeo427@gmail.com>
2026-05-11 22:05:15 +0800

    WIP Trying to debug why the hell we aren't able to reach the API Gateway inside Minikube
    Currently also having errors when trying to cURL a simple GET request to the food-service via '/bin/bash' inside the api-gateway

--- backend/api-gateway/src/main/java/com/salob/api_gateway/config/GatewayRoutesConfig.java
+++ backend/api-gateway/src/main/java/com/salob/api_gateway/config/GatewayRoutesConfig.java
@@ -1,4 +1,5 @@
 package com.salob.api_gateway.config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -6,13 +7,21 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class GatewayRoutesConfig {
+    @Value("${app.routing.user-service}")
+    private String userServiceUri;
+
+    @Value("${app.routing.food-service}")
+    private String foodServiceUri;
+
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route("user-service-route", r -> r.path("/api/auth/**", "/api/users/**")
-                        .uri("lb://user-service"))
-                .route("food-service-route", r -> r.path("/api/eateries/**", "/api/foods/**")
-                        .uri("lb://food-service"))
+                .route("user-service-route", r -> r
+                        .path("/api/auth/**", "/api/users/**")
+                        .uri(userServiceUri))
+                .route("food-service-route", r -> r
+                        .path("/api/eateries/**", "/api/foods/**")
+                        .uri(foodServiceUri))
                 .build();
     }
 }
